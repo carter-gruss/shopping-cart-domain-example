c5d641eb20f11dbb2a7a1ba1110f271ab5efdd5e
Carter Gruss <carter.gruss@concordusa.com>
2020-12-30 16:12:53 -0600

    test(Produce): Add tests for findAllFruit, findAllVegetables, findFruitByName

--- src/produce/produce.service.spec.ts
+++ src/produce/produce.service.spec.ts
@@ -1,5 +1,9 @@
 import 'reflect-metadata';
-import {ALL_MOCK_FRUIT, ALL_MOCK_VEGETABLE} from './mock/produce.mock';
+import {
+  ALL_MOCK_FRUIT,
+  ALL_MOCK_VEGETABLE,
+  MOCK_PRODUCE,
+} from './mock/produce.mock';
 import {ProduceEntity} from './produce.entity';
 import {ProduceService, ProduceServiceImpl} from './produce.service';
 import {ProduceRepository} from './repository/produce-dao.interface';
@@ -49,7 +53,63 @@ describe('ProduceService', () => {
     });
   });
 
-  describe('findAllVegetables', () => {});
+  describe('findAllVegetables', () => {
+    let repository: ProduceRepository<ProduceEntity>;
+    let service: ProduceService;
 
-  describe('findFruitByName', () => {});
+    beforeEach(() => {
+      repository = new ProduceInMemoryRepository([]);
+      service = new ProduceServiceImpl(repository);
+    });
+
+    it('should return an empty array when there is no produce', async () => {
+      const vegetables = await service.findAllVegetables();
+      expect(vegetables).toBeInstanceOf(Array);
+    });
+
+    it('should return an array of vegetable when all produce is vegetables', async () => {
+      repository = new ProduceInMemoryRepository(ALL_MOCK_VEGETABLE);
+      service = new ProduceServiceImpl(repository);
+      const expectedVegetableCount = ALL_MOCK_VEGETABLE.length;
+
+      const vegetables = await service.findAllVegetables();
+      expect(vegetables).toBeInstanceOf(Array);
+      expect(vegetables.length).toBe(expectedVegetableCount);
+    });
+
+    it('should return an empty array when all produce is fruit', async () => {
+      repository = new ProduceInMemoryRepository(ALL_MOCK_VEGETABLE);
+      service = new ProduceServiceImpl(repository);
+
+      const vegetables = await service.findAllFruit();
+      expect(vegetables).toBeInstanceOf(Array);
+      expect(vegetables.length).toBe(0);
+    });
+  });
+
+  describe('findFruitByName', () => {
+    let repository: ProduceRepository<ProduceEntity>;
+    let service: ProduceService;
+
+    beforeEach(() => {
+      repository = new ProduceInMemoryRepository([]);
+      service = new ProduceServiceImpl(repository);
+    });
+
+    it('should return null when there is no produce', async () => {
+      const produce = await service.findFruitByName('fruit');
+      expect(produce).toBeNull();
+    });
+
+    it('should return a produce whose name matches', async () => {
+      const mockProduce = MOCK_PRODUCE[2];
+      repository = new ProduceInMemoryRepository(MOCK_PRODUCE);
+      service = new ProduceServiceImpl(repository);
+      const mockProduceName = mockProduce.name;
+
+      const foundProduce = await service.findFruitByName(mockProduceName);
+      expect(foundProduce).toBeDefined();
+      expect(foundProduce?.name).toBe(mockProduceName);
+    });
+  });
 });
